67c9ef119999fba4dcc3f5c81212cefeb6338c65
Malek <120230397+mhkamal15@users.noreply.github.com>
2023-09-05 20:53:25 -0400

    Update hassan.js

--- hassan.js
+++ hassan.js
@@ -1,46 +1,104 @@
-const Discord = require('discord.js')
-const axios = require('axios')
-const client = new Discord.Client()
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildIntegrations
+  ],
+});
 
-const PREFIX = '/'
+const fetch = require('node-fetch');
+
+const jokes = [
+  'Why did the tomato turn red? Because it saw the salad dressing!', 
+  'Why was the math book sad? Because it had too many problems.', 
+  'What do you call fake spaghetti? An impasta.', 
+  'What do you call a bear with no teeth? A gummy bear.', 
+  'Why don’t scientists trust atoms? Because they make up everything.'
+];
+
+const facts = [
+  'Pakistan is the world’s 6th most populous country.', 
+  'Pakistan’s official name is “Islamic Republic of Pakistan”', 
+  'Pakistan was first created on August 14, 1947.', 
+  'Pakistan is the only Muslim nuclear power country.', 
+  'Shahid Afridi has hit 476 sixes in his international cricket career.'
+];
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+  console.log("I'm in");
+  console.log("Hassan Bot");
+});
 
-client.on('message', async message => {
-  if (message.author.bot) return
-  if (message.content.startsWith(PREFIX)) {
-    const command = message.content.slice(PREFIX.length).trim().split(/ +/g)[0]
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/g).slice(1)
-    switch (command) {
-      case 'rap':
-        message.channel.send('**Rap beat dropping in 3...2...1...**\nhttps://www.youtube.com/watch?v=PsO6ZnUZI0g')
-        break
-      case 'fact':
-        try {
-          const response = await axios.get('https://uselessfacts.jsph.pl//random.json?language=en')
-          const fact = response.data.text
-          message.channel.send(`**Did you know?**: ${fact}`)
-        } catch (error) {
-          console.error(error)
-        }
-        break
-      case 'talk':
-        const statement = args.join(' ')
-        message.channel.send(`I hear you say "${statement}". Hmmmm... 🤔`)
-        break
-      default:
-        if (message.content.toLowerCase().includes('kys')) {
-          message.channel.send('You too')
-        } else if (message.content.toLowerCase().includes('you\'re dumb')) {
-          message.channel.send('STFU, retard')
-        } else if (message.content.toLowerCase().includes('hello') || message.content.toLowerCase().includes('hi')) {
-          message.channel.send('Well, hello there!')
-        }
-        break
-    }
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) {
+    return;
   }
-})
 
-client.login('Bot-Token')
+  const { commandName } = interaction;
+
+  if (commandName === 'info') {
+    await interaction.reply('Hi! My name is Hassan Bot. Named after Mohammed "Hassan" Khan, a famed member of the community and a dear friend of the creator. I was created by Malek Kamal, who worked day and night to make me work the way I do! I am currently in development version 1.0. Thanks for listening to me!');
+  } else if (commandName === 'joke') {
+    const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+    await interaction.reply(randomJoke);
+  } else if (commandName === 'fact') {
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    await interaction.reply(randomFact);
+  } else if (commandName === 'math') {
+    const num1 = parseInt(interaction.options.getString('num1'));
+    const num2 = parseInt(interaction.options.getString('num2'));
+    const operator = interaction.options.getString('operator');
+
+    let result;
+
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num1 / num2;
+        break;
+      default:
+        result = 'Invalid operator';
+        break;
+    }
+
+    await interaction.reply(result.toString());
+  }
+});
+
+client.on('messageCreate', async message => {
+  const msg = message.content.toLowerCase();
+
+  if ((msg.includes('hassan')) && (message.author.id !== client.user.id)) {
+    await message.reply('Who summoned me?');
+  }
+
+  if ((msg.includes('shut up')) && (message.author.id !== client.user.id)) {
+    await message.reply('I am not supposed to respond to that.');
+  }
+});
+
+client.login(process.env.DISCORD_BOT_SECRET);
+
+const keepAlive = () => {
+setInterval(() => {
+    const url = 'https://discord.com/api/v8/users/@me/channels';
+    fetch(url, {
+      method: 'GET',
+      headers: {
+      Authorization: `Bot ${process.env.DISCORD_BOT_SECRET}`,
+      },
+    })
+    .then(res => {
+      console.log('pinged');
+    });
+  })}
